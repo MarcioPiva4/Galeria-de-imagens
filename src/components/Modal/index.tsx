7fdb875bf45188722f5e81b1aba111378b9f574d
Márcio Piva <110309311+MarcioPiva4@users.noreply.github.com>
2024-10-20 22:33:45 -0300

    Feat: overlay

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -18,7 +18,7 @@ const overlays = [
   { name: 'Nenhum', url: '' },
   { name: 'Emoji Corações', url: 'https://firebasestorage.googleapis.com/v0/b/livros-15d40.appspot.com/o/filtros%2Fpngtree-lovers-emoticons-background-png-image_2859444-removebg-preview.png?alt=media&token=4282d319-cbe2-4639-85e9-0b24dfd3b9f1' },
   { name: 'Emoji Feliz', url: 'https://firebasestorage.googleapis.com/v0/b/livros-15d40.appspot.com/o/filtros%2Fworld-smile-day-emojis-removebg-preview.png?alt=media&token=00f63d4f-dfec-4c35-aaee-c155151ecc5f' },
-  { name: 'Beleza', url: 'https://firebasestorage.googleapis.com/v0/b/livros-15d40.appspot.com/o/images%2Fpng-transparent-emoji-doing-thumbs-up-illustration-emoji-emoticon-smiley-like-button-thumb-signal-double-happiness-computer-icons-text-messaging-smile-thumbnail-removebg-preview.png?alt=media&token=5fe5be5c-f0a5-455e-8e82-2ae96f64e65a' }
+  { name: 'Taxi', url: 'https://firebasestorage.googleapis.com/v0/b/livros-15d40.appspot.com/o/filtros%2FDALL%C2%B7E%202024-10-20%2022.30.24%20-%20A%20white%20blank%20center%20surrounded%20by%20multiple%20yellow%20taxi%20cabs%20arranged%20as%20a%20border%20around%20the%20image%2C%20creating%20a%20filter-like%20effect.%20The%20taxis%20are%20posit.webp?alt=media&token=d07b7570-5312-425a-b6f2-a2d66b2cf96e' }
 ];
 
 const effects = [
